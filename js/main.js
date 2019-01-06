b6222f017bd0f9c5bd9ae4d63d6a44c57235c9e5
Dzianis Sheka <dzianis.sheka@gmail.com>
2019-01-06 22:34:27 +0300

    add cross frame  communication

---
+++ js/main.js
@@ -0,0 +1,38 @@
+console.log("init main");
+// alert('main');
+window.addEventListener("message", receiveMessage, false);
+
+function receiveMessage(event) {
+  // console.log("all event - inner html: ", event.origin,  event);
+
+  if (
+    event.origin !== "https://davojta-test-google-auth.github.io" &&
+    event.origin !== "http://127.0.0.1:8080"
+  )
+    return;
+
+  console.log("after check - inner html: ", event.origin, event);
+
+  if (event.data === "getAccessToken") {
+    var user = gapi.auth2.getAuthInstance().currentUser.get();
+    var oauthToken = user.getAuthResponse().access_token;
+
+    console.log("oauthToken", oauthToken);
+    event.source.postMessage(
+      {
+        type: "answer",
+        commandName: "getAccessToken",
+        payload: oauthToken
+      },
+      event.origin
+    );
+  }
+
+  event.source.postMessage("answer_from_iframe", event.origin);
+
+  // ...
+}
+
+// setTimeout(() => {
+//     window.postMessage('')
+// }, 5000);
